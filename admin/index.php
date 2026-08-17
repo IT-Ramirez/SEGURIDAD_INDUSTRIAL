3f72f63a921f4a3ccc120d26a0ef6740c6c8d928
Joel_Ramirez <v-porfirio.ramirez@equinoxgold.com>
2026-08-17 01:14:54 -0600

    manejando esatdo

--- admin/index.php
+++ admin/index.php
@@ -265,7 +265,7 @@ $inspecciones = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                             <td><?= htmlspecialchars($insp['nombre_conductor']) ?></td>
                                             <td><?= number_format((float)$insp['odometro']) ?> KM</td>
                                             <td>
-                                                <?php if ($insp['estado'] === 'Aprobado'): ?>
+                                                <?php if ($insp['estado'] === 'APTO PARA CONDUCIR'): ?>
                                                     <span class="badge bg-success-subtle text-success border border-success-subtle px-2 py-1">Aprobado</span>
                                                 <?php else: ?>
                                                     <span class="badge bg-danger-subtle text-danger border border-danger-subtle px-2 py-1">Con Fallas</span>
